d11c4ad71ba87892060db823b2dc31a272491c94
Влада Куприенко <kuprienkovladislava@gmail.com>
2026-02-07 18:54:41 +0300

    Внесла изменения в 18ДЗ

--- DZ18/DZ18.sql
+++ DZ18/DZ18.sql
@@ -1,16 +1,15 @@
 
 --Рейсы в бизнес-классе, вылетающие из Москвы
-select 
-	f.flight_no,
-	a.city,
-	a.airport_name,
-	f.scheduled_departure, 
-	s.fare_conditions
+select distinct
+    f.flight_no,
+    a.city,
+    a.airport_name,
+    f.scheduled_departure,
+    s.fare_conditions
 from flights f
-left join airports a  on  f.departure_airport = a.airport_code
-left join seats s  on f.aircraft_code  = s.aircraft_code  
+join airports a on f.departure_airport = a.airport_code
+join seats s on f.aircraft_code = s.aircraft_code
 where a.city = 'Москва' and s.fare_conditions = 'Business'
-
 
 
 --Самолеты, которые никогда не использовались в полетах
@@ -20,14 +19,4 @@ select
 from aircrafts a 
 left join flights f on a.aircraft_code = f.aircraft_code
 where f.flight_id is null
-group by a.aircraft_code 
-
-
-
-
-
-
-
-
-
-
+--group by точно не нужен
